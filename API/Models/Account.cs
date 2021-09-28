6ff39df14f657d599f3a89a9e0047cdcb1700f3e
Yepta Zagarino Samosir <yeptasamosir1906@gmail.com>
2021-09-28 15:27:36 +0700

    main

---
+++ API/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    [Table("tb_m_Account")]
+    public class Account
+    {
+        [Key]
+        public int Account_id { get; set; }
+        public string Account_Name { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
